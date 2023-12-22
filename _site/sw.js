b0645f4080eef8e339ee36c1ac549cce7a281ff5
yoon828 <yoon828990@gmail.com>
2023-12-23 01:06:26 +0900

    feat: setting test

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703246134';
+const cacheName = 'chirpy-1703261041';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
