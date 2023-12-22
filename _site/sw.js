8fd8696f00dd004565c67e0e91e71abf4ef707f6
yoon828 <yoon828990@gmail.com>
2023-12-22 19:30:08 +0900

    fix: bug fix

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703239524';
+const cacheName = 'chirpy-1703240906';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
